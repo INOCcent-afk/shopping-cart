97377717959d7a4749556d5f99aa29f6e307b713
INOCcent-afk <michael828inoc@gmail.com>
2020-11-01 16:45:02 +0800

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const addToCart = document.querySelectorAll(".purchase");
 const shop = document.querySelector(".shop");
 const cartItems = document.querySelector(".cart-items");
 const clear = document.querySelector(".removeAll");
+const item = document.querySelectorAll(".item");
 
 addToCart.forEach((add) => {
   add.addEventListener("click", showCart);
@@ -14,8 +15,8 @@ cartCloseWindow.addEventListener("click", closeCart);
 
 // cartClose
 function closeCart() {
-  cartWindow.style.transform = "translateX(100%)";
-  shop.style.padding = "100px 5% 100px 5%";
+  cartWindow.classList.remove("cartWindowStyle");
+  shop.classList.remove("shopStyle");
 }
 
 // cartOpen
@@ -23,20 +24,33 @@ let cartCount = 1;
 
 function showCart(e) {
   cartNum.innerHTML = cartCount;
-  cartWindow.style.transform = "translateX(0)";
-  shop.style.padding = "100px 400px 100px 5%";
+  cartWindow.classList.add("cartWindowStyle");
+  shop.classList.add("shopStyle");
+  e.target.disabled = true;
 
   let itemsInfo = e.target.parentNode;
   let addedItemP = document.createElement("p");
   let addedItemH = document.createElement("h1");
   let addedItem = document.createElement("div");
+  let addQuantity = document.createElement("button");
+  let substractQuantity = document.createElement("button");
+  let removeItem = document.createElement("button");
 
   itemsInfoHeading = itemsInfo.childNodes[1].textContent;
   itemsInfoPrice = itemsInfo.childNodes[3].textContent;
 
   addedItem.className = "addedItem";
+  addQuantity.className = "addQuantity";
+  substractQuantity.className = "substractQuantity";
+  removeItem.className = "removeItem";
+
   addedItemP.textContent = itemsInfoPrice;
   addedItemH.textContent = itemsInfoHeading;
+  addedItemH.className = addedItemH.textContent.toLowerCase();
+  addQuantity.innerHTML = "+";
+  substractQuantity.innerHTML = "-";
+  removeItem.innerHTML = "remove";
+
   addedItem.style.height = "100px";
   addedItem.style.width = "300px";
   addedItem.style.margin = "20px 0";
@@ -44,11 +58,25 @@ function showCart(e) {
   addedItem.appendChild(addedItemH);
   addedItem.appendChild(addedItemP);
   cartItems.appendChild(addedItem);
+  addedItem.appendChild(addQuantity);
+  addedItem.appendChild(substractQuantity);
+  addedItem.appendChild(removeItem);
+
+  removeItem.addEventListener("click", (e) => {
+    e.target.parentNode.style.display = "none";
+    let checkName = e.target.parentNode.childNodes[0].className;
+    let arr = [];
+    item.forEach((itm) => {
+      arr.push(itm.classList[1]);
+    });
+
+    console.log(arr);
+  });
 
   cartCount++;
 }
 
 cartNum.addEventListener("click", () => {
-  cartWindow.style.transform = "translateX(0)";
-  shop.style.padding = "100px 400px 100px 5%";
+  cartWindow.classList.toggle("cartWindowStyle");
+  shop.classList.toggle("shopStyle");
 });
